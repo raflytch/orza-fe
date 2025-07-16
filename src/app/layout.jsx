3d7ae973891c6ae6a391fd661337cfd263e5cd72
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2025-07-17 04:06:15 +0700

    Refactor RootLayout styles for improved background and layout consistency

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -17,11 +17,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${poppins.variable} antialiased bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]`}
-        style={{
-          minHeight: "100vh",
-        }}
+        className={`${poppins.variable} antialiased bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem] relative min-h-screen`}
       >
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
         <Navbar />
         {children}
       </body>
